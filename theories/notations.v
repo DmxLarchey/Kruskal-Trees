90d5fd71a0212f0e07869080bda7d21027cd39f8
Dominique Larchey-Wendling <larchey@gmail.com>
2022-11-16 20:04:48 +0100

    Translation layer for 8.16 hopefully works for 8.1[45] as well (#3)
    * translation layer for 8.16 hopefully works for 8.1[45] as well
    
    * solved the compatibility issues with lia ... the tank to crush a fly
    
    * Another compatibility issue
    
    * readded coq dev in the CI
    
    * forgot to allow dev in the dependencies

--- theories/notations.v
+++ theories/notations.v
@@ -8,7 +8,7 @@
 (**************************************************************)
 
 From Coq
-  Require Import List.
+  Require Import Arith Lia List.
 
 (** For lists *)
 
@@ -27,4 +27,17 @@ Arguments app {_}.
 #[global] Notation "P '∩₂' Q" := (fun x y => P x y /\ Q x y) (at level 1, no associativity, format "P ∩₂ Q").
 
 #[global] Notation "Q '∘' P" := (fun x z => exists y, P x y /\ Q y z) (at level 1, left associativity, format "Q ∘ P").
+
+(** We use lia to avoid incompatibilities between Coq <= 8.15.2 and Coq >= 8.16 
+    DLW: I know this is a like a tank to crush a fly but it is more compatible
+         than the other options *)
+Definition plus_assoc n m p : n + (m + p) = n + m + p.      Proof. lia. Qed.
+Definition le_plus_l n m : n <= n + m.                      Proof. lia. Qed.
+Definition le_plus_r n m : m <= n + m.                      Proof. lia. Qed.
+Definition le_trans n m p : n <= m -> m <= p -> n <= p.     Proof. lia. Qed.
+Definition lt_le_trans n m p : n <= m -> m <= p -> n <= p.  Proof. lia. Qed.
+Definition lt_0_Sn n : 0 < S n.                             Proof. lia. Qed.
+Definition lt_n_S n m : n < m -> S n < S m.                 Proof. lia. Qed.
+Definition lt_S_n n m : S n < S m -> n < m.                 Proof. lia. Qed.
+
 
